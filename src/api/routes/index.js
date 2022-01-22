85c08b89632a9b8488a4aaad9a93bed1f71bb7ca
Joaquin <joaquin.torres@unmsm.edu.pe>
2022-01-21 19:54:20 -0500

    ENDPOINTS DE DASHBOARD

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -1,11 +1,12 @@
 const auth = require("./auth")
+const dashboard = require("./dashboard")
 const express = require("express")
 const router = express.Router()
 
 
 //ROUTES
 router.use("/auth", auth)
-
+router.use("/dashboard", dashboard)
 
 
 //EXPORT
